7a64e5a9a4ca73986d509860e076167c3435e264
DaniB555 <bartos.daniel@student.ms.sapientia.ro>
2025-03-19 10:58:45 +0200

    labor4 update

--- labor-04/src/lab4_1/Main.java
+++ labor-04/src/lab4_1/Main.java
@@ -1,4 +1,25 @@
 package lab4_1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        readFromTextFile("beolvas.txt");
+    }
+    public static void readFromTextFile(String filename){
+        try(Scanner scanner = new Scanner(new File(filename))){
+            int lineCounter=0;
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                lineCounter++;
+                System.out.println(lineCounter +" "+line);
+            }
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 }
